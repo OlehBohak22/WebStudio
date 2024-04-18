b256f5fcf893a5b133dc4fc2783e563f9ae5e628
Oleh Bohak <eddrfgthyjn@gmail.com>
2024-04-18 15:37:53 +0300

    New commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 document.querySelector(".hero-btn").onclick = function () {
-  document.querySelector(".modal-overlay ").classList.add("is-open");
+  document.querySelector(".modal-overlay ").classList.toggle("is-open");
 };
 
 document.querySelector(".modal-close-btn").onclick = function () {
-  document.querySelector(".modal-overlay").classList.remove("is-open");
+  document.querySelector(".modal-overlay").classList.toggle("is-open");
 };
 
 document.querySelector(".header-btn").onclick = function () {
-  document.querySelector(".menu-overlay ").classList.add("is-open");
+  document.querySelector(".menu-overlay ").classList.toggle("is-open");
 };
 
 document.querySelector(".close-btn").onclick = function () {
-  document.querySelector(".menu-overlay").classList.remove("is-open");
+  document.querySelector(".menu-overlay").classList.toggle("is-open");
 };
